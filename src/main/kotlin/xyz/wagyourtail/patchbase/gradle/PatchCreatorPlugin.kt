2d545d35189b0ebbd3829b05069163b4254346ab
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-11-21 22:41:33 -0700

    refactor to allow working with other sourceSets

--- src/main/kotlin/xyz/wagyourtail/patchbase/gradle/PatchCreatorPlugin.kt
+++ src/main/kotlin/xyz/wagyourtail/patchbase/gradle/PatchCreatorPlugin.kt
@@ -7,49 +7,68 @@ import xyz.wagyourtail.patchbase.gradle.tasks.ApplySourcePatchTask
 import xyz.wagyourtail.patchbase.gradle.tasks.CreateClassPatchTask
 import xyz.wagyourtail.patchbase.gradle.tasks.CreateSourcePatchTask
 import xyz.wagyourtail.unimined.api.minecraft.EnvType
+import xyz.wagyourtail.unimined.api.minecraft.MinecraftConfig
 import xyz.wagyourtail.unimined.api.unimined
 import xyz.wagyourtail.unimined.internal.minecraft.MinecraftProvider
+import xyz.wagyourtail.unimined.internal.minecraft.patch.NoTransformMinecraftTransformer
+import xyz.wagyourtail.unimined.internal.minecraft.patch.jarmod.JarModAgentMinecraftTransformer
 import xyz.wagyourtail.unimined.util.sourceSets
+import xyz.wagyourtail.unimined.util.withSourceSet
 
 class PatchCreatorPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
-        target.tasks.register("createSourcePatch", CreateSourcePatchTask::class.java) {
-            it.group = "patchbase"
-            it.sourceDir.set(target.file("src/main/java"))
-            it.outputDir.set(target.file("patches/main"))
-            val mc = target.unimined.minecrafts[target.sourceSets.named("main").get()].minecraftFileDev
-            val sourceFile = mc.resolveSibling(mc.nameWithoutExtension + "-sources.jar")
-            it.sources.set(target.files(sourceFile))
-            if (!sourceFile.exists()) {
-                it.dependsOn("genSources")
-            }
-        }
-        target.tasks.register("applySourcePatch", ApplySourcePatchTask::class.java) {
-            it.group = "patchbase"
-            it.patchDir.set(target.file("patches/main"))
-            it.outputDir.set(target.file("src/main/java"))
-            val mc = target.unimined.minecrafts[target.sourceSets.named("main").get()].minecraftFileDev
-            val sourceFile = mc.resolveSibling(mc.nameWithoutExtension + "-sources.jar")
-            it.sources.set(target.files(sourceFile))
-            if (!sourceFile.exists()) {
-                it.dependsOn("genSources")
-            }
-        }
-        target.tasks.register("createClassPatch", CreateClassPatchTask::class.java) {
-            it.group = "patchbase"
-            it.inputFile.set((target.tasks.findByName("remapJar") as Jar).outputs.files.singleFile)
-            val mc = target.unimined.minecrafts[target.sourceSets.named("main").get()]
-            when (mc.side) {
-                EnvType.CLIENT -> it.classpath.set(target.files(mc.minecraftData.minecraftClientFile))
-                EnvType.SERVER -> it.classpath.set(target.files(mc.minecraftData.minecraftServerFile))
-                EnvType.COMBINED -> it.classpath.set(target.files(mc.mergedOfficialMinecraftFile))
-                else -> throw IllegalStateException("Unknown side: ${mc.side}")
-            }
-            it.archiveClassifier.set("patch")
-            it.dependsOn("remapJar")
+    }
 
+}
+
+fun MinecraftConfig.patchBaseCreator() {
+    if (side == EnvType.COMBINED) {
+        project.logger.warn("Merged may make applying patches more difficult, proceed with caution")
+    }
+    if (!defaultRemapJar) {
+        project.logger.warn("defaultRemapJar is false, this may cause issues with patching")
+    }
+    if (mcPatcher !is JarModAgentMinecraftTransformer) {
+        project.logger.warn("mcPatcher is not a JarModAgentMinecraftTransformer, this may cause issues with dev runs")
+    }
+
+    project.tasks.register("createSourcePatch".withSourceSet(sourceSet), CreateSourcePatchTask::class.java) {
+        it.group = "patchbase"
+        it.sourceDir.set(project.file("src/main/java"))
+        it.outputDir.set(project.file("patches/main"))
+        val sourceFile = minecraftFileDev.resolveSibling(minecraftFileDev.nameWithoutExtension + "-sources.jar")
+        it.sources.set(project.files(sourceFile))
+        if (!sourceFile.exists()) {
+            it.dependsOn("genSources")
         }
     }
 
+    project.tasks.register("applySourcePatch".withSourceSet(sourceSet), ApplySourcePatchTask::class.java) {
+        it.group = "patchbase"
+        it.patchDir.set(project.file("patches/main"))
+        it.outputDir.set(project.file("src/main/java"))
+        val sourceFile = minecraftFileDev.resolveSibling(minecraftFileDev.nameWithoutExtension + "-sources.jar")
+        it.sources.set(project.files(sourceFile))
+        if (!sourceFile.exists()) {
+            it.dependsOn("genSources".withSourceSet(sourceSet))
+        }
+    }
+
+    project.tasks.register("createClassPatch", CreateClassPatchTask::class.java) {
+        it.group = "patchbase"
+        it.inputFile.set((project.tasks.findByName("remapJar".withSourceSet(sourceSet)) as Jar).outputs.files.singleFile)
+
+        when (side) {
+            EnvType.CLIENT -> it.classpath.set(project.files(minecraftData.minecraftClientFile))
+            EnvType.SERVER -> it.classpath.set(project.files(minecraftData.minecraftServerFile))
+            EnvType.COMBINED -> {
+                it.classpath.set(project.files(mergedOfficialMinecraftFile))
+            }
+            else -> throw IllegalStateException("Unknown side: $side")
+        }
+
+        it.archiveClassifier.set("patch")
+        it.dependsOn("remapJar".withSourceSet(sourceSet))
+    }
 }
